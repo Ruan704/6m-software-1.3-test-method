a4a0c65d1caa533837449bfe1cd5cdddf96ee6cd
Joseph Ruan <63382739+Ruan704@users.noreply.github.com>
2024-09-01 20:33:16 +0800

    Update q3-objects.js

--- assignment/q3-objects.js
+++ assignment/q3-objects.js
@@ -25,6 +25,11 @@ function convert(keyArr, valueArr){
         - Step 3: Within the for-loop, add the key and value to the local scoped object.
         - Step 4: Write a return statement to return the object literal after the for-loop code block.
     */
+            const localBooksObjLength = {};
+        for (let i = 0; i < keyArr.length; i++){
+            localBooksObjLength[keyArr[i]] = valueArr[i];
+        }
+        return localBooksObjLength;
 }
 
 function printByKey(key){
